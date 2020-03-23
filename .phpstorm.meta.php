dca4b6f4f817e8b9090919ce1397e41b05c1dcd6
Tim Haak <tim@haak.co>
2020-03-23 11:07:37 +0200

    Update to laravel 7.2

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-19 13:18:19.
+    * Generated on 2020-03-23 08:57:32.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
