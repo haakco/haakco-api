aa4ae4a120d937d0f4adacf9ee68903c2086a3fa
Tim Haak <tim@haak.co>
2020-04-04 19:54:52 +0200

    Remove websocket config as not used

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-04-04 11:38:16.
+    * Generated on 2020-04-04 17:40:18.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
