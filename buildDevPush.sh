13db1f23c8bd2247ccfce134705dc1cd6f03a8d8
Tim Haak <tim@haak.co>
2020-04-10 14:56:31 +0200

    Fixing dev build

--- buildDevPush.sh
+++ buildDevPush.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 IMAGE_NAME=haakco/haakco-api
-docker build --pull --rm -t "${IMAGE_NAME}" .
+docker build --pull --file Dockerfile.dev --rm -t "${IMAGE_NAME}" .
 docker push "${IMAGE_NAME}"
