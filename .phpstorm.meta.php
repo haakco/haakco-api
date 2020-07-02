10ff64712c7fa494e0f7b2ddee9c174a542d36ac
Tim Haak <tim@haak.co>
2020-07-02 15:00:30 +0200

    Update ide helpe

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-07-02 11:42:42.
+    * Generated on 2020-07-02 12:47:24.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
